09ae0150c7150ec569a6268abf9d10e168b890fb
Steven Abreu <stevenabreu7@gmail.com>
2021-10-13 19:23:33 +0200

    basic conceptor, not performing *super* well

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -12,10 +12,3 @@ def n_periodic(n, T, key, minval=-1., maxval=1.):
 def n_sine(n, T, amplitude=1., phase=0.):
     x = np.arange(0, T) * 2 * np.pi / n
     return amplitude * np.sin(x - 2 * np.pi * phase).reshape(-1, 1)
-
-
-def concatenate_patterns(xt, washout, shift=0):
-    return np.concatenate([
-        xt[i][washout + shift: xt[i].shape[0]+shift, :]
-        for i in range(len(xt))
-    ])
